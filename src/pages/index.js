4b7544c079772e4db61e9dc80fe4ba9d6b093a48
fmmricardo <ricardofilipe5@sapo.pt>
2018-08-27 12:35:22 +0100

    Cv update

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,7 +6,6 @@ import "../assets/css/main.css";
 import Header from "../components/Header";
 import About from "../components/About";
 import Resume from "../components/Resume";
-import Portfolio from "../components/Portfolio";
 import Footer from "../components/Footer";
 
 const IndexPage = () => (
